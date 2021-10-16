16c939c10f8aaf5566fbbc5a2ac545c47fd90e9d
himanshudce <himanshu.dce12@gmail.com>
2021-10-16 16:16:53 +0200

    adding base tpcds repo

---
+++ load_data.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <scale factor>"
+	exit 1
+fi
+
+SF=$1
+DATADIR='tpcdata'
+
+if [ ! -d $DATADIR ]
+then
+	echo "Data at $DATADIR not found"
+	exit 1
+fi
+
+
+USER=root
+MYSQL="mysql -u $USER"
+
+echo "# Create database"
+$MYSQL -e "create database tpcds"
+MYSQL="$MYSQL tpcds"
+
+echo "# Create tables"
+$MYSQL < ./tpcds_kit/tools/tpcds.sql
+
+TOTAL_MSECS=0
+
+echo "# Load data into table"
+for f in `ls $DATADIR`
+do
+	t=`echo $f | sed -e "s/_[0-9]_[0-9]//"`
+	t=`echo $t | sed -e "s/.dat//"`
+	f="./$DATADIR/"$f
+	$MYSQL -e "LOAD DATA LOCAL INFILE '$f' INTO TABLE $t FIELDS TERMINATED BY '|';"
+	if [ $? -ne 0 ]
+	then
+		echo "FAIL"
+		exit 1
+	fi
+	printf "loaded the table \n" 
+done
